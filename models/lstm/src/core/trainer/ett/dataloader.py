78a7a41efd33afca7620c2209ff2984134e4eeef
eswarashish <ashish.kusumanchi@gmail.com>
2026-03-03 19:10:45 +0530

    feat(models): setup device agnostic basic setup

--- models/lstm/src/core/trainer/ett/dataloader.py
+++ models/lstm/src/core/trainer/ett/dataloader.py
@@ -1,3 +1,22 @@
 from torch.utils.data import DataLoader
-# from src.core.trainer.ett.dataset import ett_data
+from torch import device
+from src.core.trainer.ett.dataset import get_ettdata
+from pathlib import Path
+from pydantic import BaseModel
+
+class DataloaderRequest(BaseModel):
+    device: device
+    path:Path
+    batch_size:int 
+    shuffle: bool
+    num_workers:int
+
+
+def dataloader(request: DataloaderRequest)->DataLoader:
+    return DataLoader(dataset=get_ettdata(request.path,request.device),
+                      batch_size=request.batch_size,
+                      shuffle=request.shuffle,
+                      num_workers=request.num_workers,
+                      pin_memory= (request.device.type == ("cuda" or "mps"))
+                      )
 
