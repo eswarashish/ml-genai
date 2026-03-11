edef9501fa4527a96d2737c96e51f12b6eb62706
eswarashish <ashish.kusumanchi@gmail.com>
2026-03-11 22:05:52 +0530

    feat(models): add proper pydantic support and dataclass support for base trainer

--- models/lstm/src/core/trainer/state.py
+++ models/lstm/src/core/trainer/state.py
@@ -1,10 +1,34 @@
 from torch import device
 from pathlib import Path
 from pydantic import BaseModel
+from dataclasses import dataclass
+from torch import nn
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
+from torch.utils.data import DataLoader
 
-class DataloaderRequest(BaseModel):
-    device: device
-    path:Path
-    batch_size:int 
-    shuffle: bool
-    num_workers:int
+# class DataloaderRequest(BaseModel):
+#     device: device
+#     path:Path
+#     batch_size:int 
+#     shuffle: bool
+#     num_workers:int
+
+
+# class TrainerAPIRequest(BaseModel):
+#     epochs: int
+#     optimizer: Optimizer
+#     loss:nn.Module 
+#     scheduler:  LRScheduler
+#     train_dataloader: DataLoader
+#     validate_dataloader: DataLoader 
+
+
+@dataclass
+class TrainerRequest:
+    epochs: int
+    optimizer: Optimizer
+    loss:nn.Module 
+    scheduler:  LRScheduler
+    train_dataloader: DataLoader
+    validate_dataloader: DataLoader 
